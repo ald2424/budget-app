b91efee24a2cb1ba3083b05603e8f676ca0de076
ald2424 <abigaild2424@gmail.com>
2020-02-29 19:22:53 -0500

    trying to get heroku to work..

--- BudgetTracker/BudgetTracker/server.js
+++ BudgetTracker/BudgetTracker/server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-var MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/budget'
+var MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/mongoHeadlines';
 
 mongoose.connect(MONGODB_URI);
 //   , {
