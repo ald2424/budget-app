67119b07b5fcd9f6cb2f633605a69cd119086b21
ald2424 <abigaild2424@gmail.com>
2020-03-09 18:19:25 -0400

    rearranging files

--- server.js
+++ server.js
@@ -17,11 +17,10 @@ app.use(express.static("public"));
 
 var MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/mongoHeadlines';
 
-mongoose.connect(MONGODB_URI);
-//   , {
-//   useNewUrlParser: true,
-//   useFindAndModify: false
-// });
+mongoose.connect("mongodb://localhost/budget", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
 
 app.use(require("./routes/api.js"));
 
